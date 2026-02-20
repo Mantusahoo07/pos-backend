c2e5dd75d5bf0572d945f6800a93366e797612ec
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 14:15:30 +0530

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { addOrder, getOrders, getOrderById, updateOrder } = require("../controllers/orderController");
+const { 
+  addOrder, 
+  getOrders, 
+  getOrderById, 
+  updateOrder,
+  getOrdersByStatus,
+  getTodaysOrders 
+} = require("../controllers/orderController");
 const { isVerifiedUser } = require("../middlewares/tokenVerification");
 const router = express.Router();
 
-
+// Protected routes
 router.route("/").post(isVerifiedUser, addOrder);
 router.route("/").get(isVerifiedUser, getOrders);
+router.route("/today").get(isVerifiedUser, getTodaysOrders);
+router.route("/status/:status").get(isVerifiedUser, getOrdersByStatus);
 router.route("/:id").get(isVerifiedUser, getOrderById);
 router.route("/:id").put(isVerifiedUser, updateOrder);
 
