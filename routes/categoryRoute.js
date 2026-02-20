fecafb3edccd7c5d27290e76b8c74b8e8b467cfb
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 13:17:21 +0530

    Update categoryRoute.js

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -9,11 +9,8 @@ const {
     deleteCategory
 } = require("../controllers/categoryController");
 
-// Public routes (if needed)
 router.route("/").get(getCategories);
 router.route("/count").get(getCategoryCount);
-
-// Protected routes (require authentication)
 router.route("/").post(isVerifiedUser, addCategory);
 router.route("/:id").put(isVerifiedUser, updateCategory);
 router.route("/:id").delete(isVerifiedUser, deleteCategory);
