e44f31f8ce83d14732932bca485c7edb3ad420f1
Mantusahoo07 <mantusahoo.bgr@gmail.com>
2026-02-20 12:54:21 +0530

    Create menuItemRoute.js

---
+++ routes/menuItemRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { isVerifiedUser } = require("../middlewares/tokenVerification");
+const {
+    getMenuItems,
+    getMenuItemsByCategory,
+    getMenuItemCount,
+    addMenuItem,
+    updateMenuItem,
+    deleteMenuItem,
+    toggleAvailability
+} = require("../controllers/menuItemController");
+
+// Public routes
+router.route("/").get(getMenuItems);
+router.route("/count").get(getMenuItemCount);
+router.route("/category/:categoryId").get(getMenuItemsByCategory);
+
+// Protected routes
+router.route("/").post(isVerifiedUser, addMenuItem);
+router.route("/:id").put(isVerifiedUser, updateMenuItem);
+router.route("/:id").delete(isVerifiedUser, deleteMenuItem);
+router.route("/:id/toggle").patch(isVerifiedUser, toggleAvailability);
+
+module.exports = router;
